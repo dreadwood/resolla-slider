86375db87f008717c2b1058d3353153d0e1f4efc
Alexey <stereo3dcourse@gmail.com>
2024-10-01 16:14:00 +0300

    Update worker6.js

--- worker6.js
+++ worker6.js
@@ -51,6 +51,7 @@ const swiperInstanse = new Swiper('.swiper-container', {
         //console.log('datahash 3',datahash);
       }
 	    swiper.updateSlides()
+	    console.log('swiper.slides.length',swiper.slides.length, typeof swiper.slides.length);
 
       if (!swiper.isBeginning) {
         gBtnLeft?.removeAttribute('disabled', 'disabled')
