79f590f754a27a12c55a3c92f997285f08e376f6
Alexey <stereo3dcourse@gmail.com>
2024-10-02 15:39:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const username = 'smenshov'
 const photoIndex = 1
-const countSlides = 6
+let countSlides = 6
 
 const gContainer = document.querySelector('.gallery__container')
 const gBtnLeft = document.querySelector('.gallery__button-left')
